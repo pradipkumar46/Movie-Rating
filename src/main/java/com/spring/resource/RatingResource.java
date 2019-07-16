da991dca6c275e8b41ef8f36b9a1772c2ee404ef
pradipkumar <pradipkumar.java@gmail.com>
2019-07-16 23:57:59 +0530

    Movie-Rating

---
+++ src/main/java/com/spring/resource/RatingResource.java
@@ -0,0 +1,30 @@
+package com.spring.resource;
+
+import com.spring.model.Rating;
+import com.spring.model.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/rating")
+public class RatingResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+    return new Rating(movieId,4);
+    }
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings= Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678", 3)
+        );
+        UserRating userRating=new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
+    }
+}
